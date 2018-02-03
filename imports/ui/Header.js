7b25464dc8e508742601d5110310ac45b5b6d72f
Dudyak Michael <sergolwow@gmail.com>
2018-02-03 21:04:55 +0100

    Some fixes

--- imports/ui/Header.js
+++ imports/ui/Header.js
@@ -32,7 +32,7 @@ export default class Header extends Component {
                 <option value="TH-0002">TH-0002</option>
               </select>
             </div>
-            <div style={{flex:1,display:"flex",alignItems: 'stretch',margin:'3vw'}}>i: <input ref="i" type="text" style={{backgroundColor: '#333',margin:'3vw',border:0,width:'20vw'}} name="lname"/><button style={{background: '#333',border: 0,margin: '3vw',padding: 0,lineHeight: '100%',textAlign: 'center',flex: 1}} onClick={this.okClick.bind(this)}>OK</button></div>
+            <div style={{flex:1,display:"flex",alignItems: 'stretch',margin:'3vw'}}><div style={{lineHeight: '10vw',margin:'3vw',width:'20vw'}}>i: </div><input ref="i" type="number" style={{backgroundColor: '#333',margin:'3vw',border:0,width:'20vw', textAlign: 'center'}} name="lname"/><button style={{background: '#333',border: 0,margin: '3vw',padding: 0,lineHeight: '100%',textAlign: 'center',flex: 1}} onClick={this.okClick.bind(this)}>OK</button></div>
         </div>
     );
   }
@@ -41,10 +41,10 @@ export default class Header extends Component {
 const Stylesheet = {
   header: open => ({
     flex:1,
-    lineHeight: '10vh',
+    lineHeight: (open?'6vw':'10vh'),
     textAlign:'center', 
-    fontSize:'5vh',
-    height:(open? '25vh':'10vh'),
+    fontSize:(open?'6vw':'5vh'),
+    height:(open? '40vw':'10vh'),
     backgroundColor: '#080808',
     boxShadow: '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)',
     transition: 'height 1000ms',
